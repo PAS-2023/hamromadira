7fde6d0b9900a85f65a829ff548bab5acf730185
mprabesh <theprabesh723@gmail.com>
2023-11-14 21:12:56 +0545

    Revert "Revert "modified : nav""
    revert back to initial commit

--- hamromadira-frontend/src/components/NavBar.jsx
+++ hamromadira-frontend/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import Menu from "./navComponents/Menu";
 import Search from "./navComponents/SearchBar";
 const NavBar = () => {
   return (
-    <div id="nav-container">
+    <div id="nav-bar">
       <Logo />
       <Menu />
       <Search />
