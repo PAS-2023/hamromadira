8d8b00dc87f7a76708679ee7eb52e2dc7f3f590c
mprabesh <theprabesh723@gmail.com>
2023-11-23 21:52:11 +0545

    feat:signup page UI completed

---
+++ hamromadira-frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar/NavBar";
+import SignUp from "../components/SignUpComponents/SignUp";
+import Footer from "../components/FooterComponents/Footer";
+
+export default function SignUpPage() {
+  return (
+    <div>
+      <NavBar />
+      <SignUp />
+      <Footer />
+    </div>
+  );
+}
