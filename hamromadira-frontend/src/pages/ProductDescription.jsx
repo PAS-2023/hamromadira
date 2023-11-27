4278732ee0b1ed47a18bf052ba72a6ded8b266ca
GojoIsHere <sus.thanet0726@gmail.com>
2023-11-27 21:30:53 +0545

    Initilize: building product description page

---
+++ hamromadira-frontend/src/pages/ProductDescription.jsx
@@ -0,0 +1,16 @@
+import NavBar from "../components/NavBar/NavBar";
+import Footer from "../components/FooterComponents/Footer";
+
+export default function ProductDescription() {
+
+  return (
+    <div>
+      <NavBar />
+       <div style={{ height: "390px" }}>
+        <h1>Product Description Page</h1>
+        This page is under construction.
+      </div>
+      <Footer />
+    </div>
+  );
+}
