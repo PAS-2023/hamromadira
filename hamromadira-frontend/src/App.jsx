e7d5ae78c8d2a356db634aa32717e32d373326e1
GojoIsHere <sus.thanet0726@gmail.com>
2023-11-27 16:54:15 +0545

    feat:fixed landing page UI

--- hamromadira-frontend/src/App.jsx
+++ hamromadira-frontend/src/App.jsx
@@ -1,22 +1,11 @@
-import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
 import NavBar from "./components/NavBar/NavBar";
-
-import Product from "./components/Card/Product";
-import Stock from "./components/Stockproduct.jsx/Stock";
-import AdSlider from "./components/AdSlider/AdSlider";
-import TopProduct from "./components/TopProducts/TopProduct";
-import Brand from "./components/Brand/Brand";
+import LandingPage from "./pages/LandingPage/LandingPage";
 import Footer from "./components/FooterComponents/Footer";
 function App() {
   return (
     <div>
       <NavBar />
-      <AdSlider />
-      <Stock />
-      <Product />
-      <Brand />
-      <TopProduct />
+      <LandingPage />
       <Footer />
 
     </div>
--- hamromadira-frontend/src/components/Brand/Brand.jsx
+++ hamromadira-frontend/src/components/Brand/Brand.jsx
@@ -1,31 +1,30 @@
-import { useEffect, useState } from "react"
-import "./brand.css"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import "./brand.css";
+import axios from "axios";
 
 const Brand = () => {
-    const [images,setImages]=useState([])
-    useEffect(() => {
-        axios.get("http://localhost:3002/brands/").then(response => {
-            setImages(response.data) 
-        })
-    }, [])
-    return (
-        <>
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3002/brands/").then((response) => {
+      setImages(response.data);
+    });
+  }, []);
+  return (
+    <div className="brand-container">
       <div className="stock-product">
-        <img src="images/stockProduct-logo.png" className="brand-logo" alt=""/>
+        <img src="images/stockProduct-logo.png" className="brand-logo" alt="" />
         <p className="stockProduct">Brands</p>
+      </div>
+      <div className="brands">
+        {images.map((item) => {
+          return (
+            <>
+              <img src={item.image} alt="" />
+            </>
+          );
+        })}
+      </div>
     </div>
-            < div className="brands">
-                {images.map((item) => {
-                    return (
-                        <>
-                            <img src={item.image} alt="" />
-                        </>
-                    )
-                })}
-    </div>
-        </>
-    )
-    
-}
+  );
+};
 export default Brand;
