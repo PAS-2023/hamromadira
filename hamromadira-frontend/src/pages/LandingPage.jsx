dad21efa8720212d04ddc76e6b15cbbac07f57d4
mprabesh <theprabesh723@gmail.com>
2023-11-27 18:38:16 +0545

    feat:Landing page configured

--- hamromadira-frontend/src/pages/LandingPage.jsx
+++ hamromadira-frontend/src/pages/LandingPage.jsx
@@ -1,14 +1,31 @@
+import Product from "../components/Card/Product";
+import Stock from "../components/Stockproduct.jsx/Stock";
+import AdSlider from "../components/AdSlider/AdSlider";
+import TopProduct from "../components/TopProducts/TopProduct";
+import Brand from "../components/Brand/Brand";
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/FooterComponents/Footer";
-export default function LandingPage() {
+
+function LandingPage() {
+  const LandingPageCSS = {
+    "max-width": "auto",
+    "max-height": "auto",
+    margin: "auto",
+    padding: "15px",
+  };
   return (
-    <div>
+    <>
       <NavBar />
-      <div style={{ height: "390px" }}>
-        <h1>Hamro Madira App</h1>
-        This page is under construction.
+      <div className="Landin-container" style={LandingPageCSS}>
+        <AdSlider />
+        <Stock />
+        <Product />
+        <Brand />
+        <TopProduct />
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
+
+export default LandingPage;
