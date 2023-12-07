d6fa39ec8fe5ceb4a4b20b983f95348113a4db73
mprabesh <theprabesh723@gmail.com>
2023-12-07 16:50:06 +0545

    feat: checkout page initiated

---
+++ hamromadira-frontend/src/pages/CheckOutPage.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar/NavBar";
+import Footer from "../components/FooterComponents/Footer";
+
+export default function CheckOutPage() {
+  return (
+    <div>
+      <NavBar />
+      <h1>This page is under construction.</h1>
+      <Footer />
+    </div>
+  );
+}
