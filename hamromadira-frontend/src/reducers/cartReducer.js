27ce0418c5d456c4b6323644e9395c42e8b5cd5a
mprabesh <theprabesh723@gmail.com>
2023-12-08 23:42:17 +0545

    feat: cartReducer redux store setup

--- hamromadira-frontend/src/reducers/cartReducer.js
+++ hamromadira-frontend/src/reducers/cartReducer.js
@@ -1,14 +1,57 @@
+/* eslint-disable no-unused-vars */
 import { createSlice } from "@reduxjs/toolkit";
+import {
+  getCartDetail,
+  editCart,
+  deleteCartItems,
+} from "../services/carts/cart";
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: [],
   reducers: {
-    getCartItems(state, actions) {
-      return actions.payload;
+    setCartItems(state, action) {
+      return action.payload;
+    },
+    appendItem(state, action) {
+      return state.concat(action.payload);
+    },
+    editCartItems(state, action) {
+      return state.map((item) =>
+        item.skus === action.payload.skus
+          ? item.quantity === action.payload.quantity
+          : item
+      );
+    },
+    removeItem(state, action) {
+      return state.filter((item) => item.skus !== action.payload.skus);
+    },
+    cartCount(state, action) {
+      return state.length;
     },
   },
 });
 
-export const { getCartItems } = cartSlice.actions;
+export const initializeCart = (userId) => {
+  return async (dispatch) => {
+    const cartDetail = await getCartDetail(userId);
+    dispatch(setCartItems(cartDetail));
+  };
+};
+
+export const editItemsToCart = (userId, item) => {
+  return async (dispatch) => {
+    const response = await editCart(userId, item);
+    dispatch(appendItem(response));
+  };
+};
+
+export const removeCartItem = (userId, skus) => {
+  return async (dispatch) => {
+    const response = await deleteCartItems(skus);
+    dispatch(removeItem(response));
+  };
+};
+
+export const { setCartItems, appendItem, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
