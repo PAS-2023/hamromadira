d6a815a107649de41ba617f91d91e38e441606ee
GojoIsHere <sus.thanet0726@gmail.com>
2023-12-21 11:01:44 +0545

    fixed : redner problem of address bar

--- hamromadira-frontend/src/components/CheckoutComponents/Address/AdressWrapper.jsx
+++ hamromadira-frontend/src/components/CheckoutComponents/Address/AdressWrapper.jsx
@@ -11,7 +11,7 @@ const AdressWrapper = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(userInfo());
-  }, []);
+  }, [addressList]);
 
   return (
     <div className="address-wrapper">
